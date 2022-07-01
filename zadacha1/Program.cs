7584aca937804ec3bffdafb0f2127655f5a4d0f0
M.Voropaev <maks.woropaew@mail.ru>
2022-07-01 21:43:27 +0300

    complite zadacha1

--- zadacha1/Program.cs
+++ zadacha1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, 
+//  обозначающую день недели, и проверяет, является ли этот день выходным.
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите день недели: ");
+            int num_day = Convert.ToInt32(Console.ReadLine());
+            if( num_day == 1) Console.WriteLine("Понедельник");
+            if( num_day == 2) Console.WriteLine("Вторник");
+            if( num_day == 3) Console.WriteLine("Среда");
+            if( num_day == 4) Console.WriteLine("Четверг");
+            if( num_day == 5) Console.WriteLine("Пятница");
+            if( num_day == 6) Console.WriteLine("Выходной");
+            if( num_day == 7) Console.WriteLine("Выходной");
+            if( num_day >  7) Console.WriteLine("НЕ число от 1 до 7");
+        }
+    }
+}
