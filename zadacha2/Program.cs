e4d514441abd292f690a1be6007a637bd0570d57
M.Voropaev <maks.woropaew@mail.ru>
2022-07-01 21:47:20 +0300

    Add zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+           
+          
+        }
+    }
+}
