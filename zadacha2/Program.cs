2bf38a37905fd0690659d4df4aa4c280372a7e61
M.Voropaev <maks.woropaew@mail.ru>
2022-07-04 10:31:26 +0300

    zadacha2

--- zadacha2/Program.cs
+++ zadacha2/Program.cs
@@ -5,8 +5,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите число: ");
-           
+            Console.Write("Введи трёхзначное число: ");
+            int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+            string stringNumber = Convert.ToString(threeDigitNumber);
+            Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);           
           
         }
     }
